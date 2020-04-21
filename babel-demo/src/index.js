fc7dd32e24865f15609ec08610942ac99df2143e
lancer <43966458+lancertea@users.noreply.github.com>
2020-04-21 23:12:18 +0800

    update

--- babel-demo/src/index.js
+++ babel-demo/src/index.js
@@ -1,14 +1,21 @@
+//import '@babel/polyfill'
+//babel 自身解析出来是  require('@babel/polyfill'),但是不真的引入该模块，需要webpack
+
 const sum = (a, b) => a + b
 
+
+//babel-polyfill
 // 新的 API
 Promise.resolve(100).then(data => data);
 
 // 新的 API
 [10, 20, 30].includes(20)
 
+//babel只解析语法，不关心API，不处理模块化（webpack）
 // 语法，符合 ES5 语法规范
-// 不处理模块化（webpack）
 
+
+//babel-polyfill的问题
 // 污染全局环境
 // window.Promise1 = function() {}
 // Array.prototype.includes1 = function () {}
